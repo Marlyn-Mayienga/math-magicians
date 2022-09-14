f2dd7944f826671e700079c958f0c4d4302b02e8
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-09-14 15:48:33 +0300

    Add events to calculator to solve problems

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prefer-stateless-function */
 
-import React from 'react';
+import React, { Fragment } from 'react';
+import calculate from '../logic/calculate';
 
 const calcRows = [
   ['AC', '+/-', '%', '÷'],
@@ -11,26 +12,48 @@ const calcRows = [
   ['0', '.', '='],
 ];
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  performCalculation = (buttonName) => {
+    const updatedCalculation = calculate(this.state, buttonName);
+    this.setState(updatedCalculation);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+    const isStateNull = () => !total && !next && !operation;
+    const displayCalculation = () => {
+      if (operation) return `${total} ${operation} ${next || ''}`;
+      return next || total;
+    };
     return (
       <div className="calc-grid">
-        <div className="output">0</div>
+        <div className="output">{isStateNull() ? '0' : displayCalculation()}</div>
         {
-          calcRows.map((buttonsRow) => (
-            <>
+          calcRows.map((calcRows, rowIndex) => (
+            <Fragment key={`row ${rowIndex + 1}`}>
               {
-                  buttonsRow.map((buttonName, index) => (
+                  calcRows.map((buttonName, index) => (
                     <button
+                      onClick={() => { this.performCalculation(buttonName); }}
+                      type="button"
                       key={buttonName}
                       className={`calc-btn 
                       ${buttonName === '0' ? 'span-two' : ''} 
-                      ${index === buttonsRow.length - 1 ? 'operator' : ''}`}
+                      ${index === calcRows.length - 1 ? 'operator' : ''}`}
                     >
                       { buttonName }
                     </button>
                   ))
                 }
-            </>
+            </Fragment>
           ))
         }
       </div>
