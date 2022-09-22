0ac0413489237c70cf1d2b54e53051226136e083
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-09-22 18:40:45 +0300

    modify website

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,12 +4,11 @@ export default function Quote() {
   return (
     <div className="quote-section">
       <p className="quote">
-        As far as the laws of mathematics refer to reality,
-        they are not certain, and as far as they are certain,
-        they do not refer to reality.
+        To me, mathematics, computer science, and the arts are insanely related.
+        They’re all creative expressions.
       </p>
       <p className="author">
-        Richard Courant, German-American mathematician
+        Sebastian Thrun, German innovator, computer scientist and entrepreneur educator
       </p>
     </div>
   );
